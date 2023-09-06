0583bcafc581eab02e580907326d5a87358b2b50
Jack <jpat0036@student.monash.edu>
2023-09-07 00:09:23 +1000

    Adds purchase products page with dummy text and images

---
+++ src/Controller/BuyProductsController.php
@@ -0,0 +1,9 @@
+<?php
+    namespace App\Controller;
+    use App\Controller\AppController;
+    class BuyProductsController extends AppController{
+        public function index(){
+             $this->layout='mylayout';
+        }
+    }
+?>
