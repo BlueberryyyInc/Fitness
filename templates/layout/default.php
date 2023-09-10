cc6e6cdd887de0dac5b9b8b6952bc0d02ad3bb36
frecklemaid <triba.directo.gon@gmail.com>
2023-09-10 12:45:45 +1000

    made the brand in navbar clickable to home page

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -51,7 +51,7 @@ $cakeDescription = 'Superior Fitness';
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
             <div class="container px-4">
-                <a class="navbar-brand" href="#page-top">Superior Fitness</a>
+                <a class="navbar-brand" href="Customers">Superior Fitness</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto">
